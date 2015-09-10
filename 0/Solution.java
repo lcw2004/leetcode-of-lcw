5cc9cce1de02f8343cd24092381206d204e9e3da
Lcw <lcw2004@163.com>
2015-09-10 17:50:42 +0800

    添加一个模板

---
+++ 0/Solution.java
@@ -0,0 +1,6 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution {
+   
+}
---
+++ 0/Test.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+
+/**
+ * Created by Lcw on 2015/9/10.
+ */
+public class Test {
+
+    @org.junit.Test
+    public void testSolution() {
+
+    }
+
+    @org.junit.Test
+    public void testSolution1() {
+
+    }
+
+
+}
